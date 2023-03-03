f0dc6f2ac18546c37e9e9363c61dfd88869f1e29
thezzisu <thezzisu@gmail.com>
2023-03-03 11:13:55 +0800

    fix: vp on mobile devices

--- pages/help.vue
+++ pages/help.vue
@@ -1,12 +1,12 @@
 <template>
-  <div class="flex justify-center gap-4 items-start">
-    <div class="shadow p-4 flex flex-col gap-2 min-w-48">
+  <div class="flex flex-wrap-reverse justify-center gap-4">
+    <div class="shadow lt-sm:flex-1 self-end p-4 flex flex-col gap-2 min-w-64">
       <div class="text-2xl border-b">Helps</div>
       <NuxtLink v-for="item of articles" :key="item._id" :to="item._path" class="p-2 btn">
         {{ item.navTitle || item.title }}
       </NuxtLink>
     </div>
-    <div class="shadow flex-1 max-w-256 w-0 flex flex-col p-4">
+    <div class="shadow flex-1 max-w-256 min-w-64 w-0 flex flex-col p-4">
       <NuxtPage />
     </div>
   </div>
