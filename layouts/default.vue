bd5306ca1b8749395bfacd737a75f92b452a5cab
thezzisu <thezzisu@gmail.com>
2023-03-04 02:55:21 +0000

    chore: remove unnecessary class

--- layouts/default.vue
+++ layouts/default.vue
@@ -1,7 +1,7 @@
 <template>
   <div class="min-h-full relative flex flex-col items-stretch">
     <AppHeader class="sticky top-0 h-16 shadow" />
-    <main class="flex-1 m-8 box-border">
+    <main class="flex-1 m-8">
       <slot></slot>
     </main>
     <AppFooter />
