5f7c0d8d5974812d615966267575c28da4da522a
thezzisu <thezzisu@gmail.com>
2023-03-04 16:11:43 +0000

    chore: remove unnecessary comments

--- components/VariableInput.vue
+++ components/VariableInput.vue
@@ -1,19 +1,11 @@
 <template>
   <div>
     <span>{{ props.config.name }}</span>
-    <!-- <select
-      v-if="props.config.type === 'select'"
-      :value="modelValue"
-      @change="emit('update:modelValue', ($event.target as any).value)"
-    >
-      <option v-for="option in config.options" :key="option.value" :value="option.value">
-        {{ option.name }}
-      </option>
-    </select> -->
     <AppSelect
+      v-if="props.config.type === 'select'"
       :options="config.options"
       :model-value="modelValue"
-      @update:modelValue="(val) => emit('update:modelValue', val)"
+      @update:model-value="(val) => emit('update:modelValue', val)"
     />
   </div>
 </template>
