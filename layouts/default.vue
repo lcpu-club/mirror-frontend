d8e2b514f156cbee14ca042f90b6c82e32a6375d
thezzisu <thezzisu@gmail.com>
2023-03-04 02:50:23 +0000

    chore: revert changes to height impl

--- layouts/default.vue
+++ layouts/default.vue
@@ -1,16 +1,9 @@
 <template>
   <div class="min-h-full relative flex flex-col items-stretch">
-    <AppHeader class="sticky top-0 h-16 shadow box-border" />
-    <main id="main" class="flex-1 m-8 box-border">
+    <AppHeader class="sticky top-0 h-16 shadow" />
+    <main class="flex-1 m-8 box-border">
       <slot></slot>
     </main>
-    <AppFooter class="h-[10rem] box-border" />
+    <AppFooter />
   </div>
 </template>
-<script setup lang="ts"></script>
-
-<style scoped>
-#main {
-  min-height: calc(100vh - 18rem);
-}
-</style>
