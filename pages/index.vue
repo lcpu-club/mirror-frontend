8adc2141d8838f9d2333d87236a66e9787c37bd9
Yixuan Wang <tom-yixuan_wang@qq.com>
2023-03-25 13:17:56 +0800

    feat: dark mode (#18)
    * feat: dark mode
    
    * fix: mirror status color in dark mode
    
    * fix: typo in mirror-text.svg
    
    ---------
    
    Co-authored-by: Zisu Zhang <thezzisu@gmail.com>

--- pages/index.vue
+++ pages/index.vue
@@ -4,7 +4,12 @@
       <div class="flex justify-between items-center p-4 border-b">
         <div class="text-2xl">Mirror List</div>
         <div>
-          <input v-model="search" type="text" class="border border-rd px-2" placeholder="Search" />
+          <input
+            v-model="search"
+            type="text"
+            class="border border-rd px-2 dark:bg-dark-back"
+            placeholder="Search"
+          />
         </div>
       </div>
       <AsyncData :data="respRef">
@@ -115,10 +120,10 @@ const helps = computed(() =>
 }
 
 .mirrorstatus-error {
-  @apply bg-red-100;
+  @apply bg-red-100 dark:bg-red-900;
 }
 
 .mirrorstatus-sync {
-  @apply bg-blue-100;
+  @apply bg-blue-100 dark:bg-blue-900;
 }
 </style>
