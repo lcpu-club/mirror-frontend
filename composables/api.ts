d46ad50cb187c5fe1d97e3cd671deeb3c0cb2bfb
leavelet <leavelet@163.com>
2024-08-31 01:16:24 +0800

    feat: use local api server

--- composables/api.ts
+++ composables/api.ts
@@ -11,7 +11,47 @@ export interface IMirrorEntry {
   state: 'done' | 'error' | 'sync'
 }
 
-export const useMirrorList = () => useFetch<IMirrorEntry[]>('/api/mirrorlist')
+interface RepositoryInfo {
+  name: string;
+  url: string;
+  help?: string;
+  describe: string;
+}
+
+interface Repositories {
+  [id: string]: RepositoryInfo;
+}
+
+interface MirrorStatus {
+  id: string;
+  state: 'done' | 'error' | 'sync';
+  lastSyncTime: number;
+  nextSyncTime: number;
+  diskUsage: string;
+}
+
+export function useMirrorList(): Promise<IMirrorEntry[]> {
+  // fetch /monitor/status and /monitor/mirrors, then combine them by id
+  const repositories = $fetch<Repositories>('/monitor/mirrors', {});
+  const status = $fetch<MirrorStatus[]>('/monitor/status', {});
+
+  // merge repositories and status
+  return Promise.all([repositories, status]).then(([repositories, status]) => {
+      return status.map((item) => {
+          const repo = repositories[item.id];
+          return {
+              id: item.id,
+              name: repo.name,
+              desc: repo.describe,
+              url: repo.url,
+              diskUsage: item.diskUsage,
+              lastSyncTime: item.lastSyncTime,
+              nextSyncTime: item.nextSyncTime,
+              state: item.state
+          };
+      });
+  });
+}
 
 export interface IFileEntry {
   mtime: string
@@ -21,7 +61,7 @@ export interface IFileEntry {
 }
 
 export function getFileList(path: string): Promise<IFileEntry[]> {
-  return $fetch<IFileEntry[]>(`/api/filelist?path=${encodeURIComponent(path)}`, {})
+  return $fetch<IFileEntry[]>(`/files/${encodeURIComponent(path)}`, {})
 }
 
-export const useStatus = (type: string) => useFetch<StatusResultWithTime>(`/api/status/${type}`)
+export const useStatus = (type: string) => useFetch<StatusResultWithTime>(`/monitor_device_status/${type}.json`)
